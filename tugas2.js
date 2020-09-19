9fe623e3da15e39a62746522591cf080e9f6329f
Elitrirudiyati <71365611+Elitrirudiyati@users.noreply.github.com>
2020-09-20 01:46:14 +0700

    Add files via upload

---
+++ tugas2.js
@@ -0,0 +1,39 @@
+var angka = 200;
+console.log(angka);
+console.log(typeof angka);
+
+var kode = "23451000";
+console.log(kode);
+console.log(typeof kode);
+
+
+var banyak = false;
+var sedikit = true;
+var uang = 10000;
+if (uang>15000) banyak=true;
+else banyak=false;
+console.log(banyak);
+console.log(typeof banyak);
+
+
+var nama = "rike";
+nama=null;
+console.log(nama);
+console.log(typeof nama);
+
+var makanan;
+console.log(makanan);
+console.log (typeof makanan)
+
+var anekabuah = ["apel", "jeruk", "pepaya", "pisang", "jambu"];
+console.log(anekabuah);
+console.log (typeof anekabuah);
+
+
+
+
+
+
+
+
+
